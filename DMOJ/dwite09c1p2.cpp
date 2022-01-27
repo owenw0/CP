99c9b5b0682f51162e2b7fcc705fa5f2f406b1ca
Owen Wang <owenwang0099@gmail.com>
2022-01-27 15:13:00 -0500

    dwite09c1p2.cpp

---
+++ DMOJ/dwite09c1p2.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> permutations;
+
+void permute(string word, int letter, int length) {
+    if (letter == length) {
+        permutations.push_back(word);
+    }
+    else {
+        for (int i = letter; i <= length; i++) {
+            swap(word[letter], word[i]);
+            permute(word, letter + 1, length);
+            swap(word[letter], word[i]);
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string str;
+    for (int i = 0; i < 5; i++) {
+        cin >> str;
+        permute(str, 0, str.size() - 1);
+    }
+
+    sort(permutations.begin(), permutations.end(),
+        [](const string& lhs, const string& rhs) {
+            return lhs.size() == rhs.size() ?
+                lhs < rhs : lhs.size() < rhs.size(); });
+
+    for (const auto& permutation : permutations) {
+        cout << permutation << "\n";
+    }
+}
