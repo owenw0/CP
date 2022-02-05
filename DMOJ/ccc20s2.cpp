c2f0fb7209a4db376e1c2337497516558e05290b
Owen Wang <owenwang0099@gmail.com>
2022-02-04 21:12:10 -0500

    CCC 2020

--- DMOJ/ccc20s2.cpp
+++ DMOJ/ccc20s2.cpp
@@ -1,52 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Cell {
-public:
-    int r, c;
-};
-
-bool visited[1010][1010];
-vector<vector<Cell>> vals(1000010);
-
-string solution(Cell last_cell) {
-    queue<Cell> q;
-    q.push(last_cell);
-    while (!q.empty()) {
-        Cell cell = q.front();
-        q.pop();
-        vector<Cell> neighbors = vals[cell.r * cell.c];
-        for (Cell neighbor : neighbors) {
-            if (neighbor.r == 1 && neighbor.c == 1) {
-                return "yes";
-            }
-            else if (!visited[neighbor.r][neighbor.c]) {
-                visited[neighbor.r][neighbor.c] = true;
-                q.push(neighbor);
-            }
-        }
-    }
-    return "no";
-}
+vector<int> adj[1000010];
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int rows, cols; cin >> rows >> cols;
-    Cell last_cell;
+    int M, N; cin >> M >> N;
+    int x;
 
-    for (int i = 1; i <= rows; i++) {
-        for (int j = 1; j <= cols; j++) {
-            int val; cin >> val;
-            Cell cell;
-            cell.r = i; cell.c = j;
-            vals[val].push_back(cell);
-
-            if (i == rows && j == cols) {
-                last_cell = cell;
-            }
+    for (int i = 1; i <= M; i++) {
+        for (int j = 1; j <= N; j++) {
+            cin >> x;
+            adj[i * j].push_back(x);
         }
     }
-    cout << solution(last_cell) << "\n";
+
+    bool visited[1000010];
+    visited[1] = true;
+    queue<int> q;
+    q.push(1);
+
+    while (!q.empty()) {
+        int nv = q.front();
+        q.pop();
+
+        for (auto i : adj[nv]) {
+            if (!visited[i]) {
+                q.push(i);
+                visited[i] = true;
+            }
+        }
+
+        if (nv == M * N) {
+            cout << "yes\n";
+            return 0;
+        }
+    }
+
+    cout << "no\n";
 }
