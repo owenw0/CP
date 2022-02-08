7b2bb4a6de80bae92d9815e7738bf6d8da67b9e3
Owen Wang <owenwang0099@gmail.com>
2022-02-07 21:44:14 -0500

    added isPrime.cpp

--- Resources/isPrime.cpp
+++ Resources/isPrime.cpp
@@ -2,21 +2,25 @@
 using namespace std;
 
 bool isPrime(int n) {
-    if (n <= 1)
+    if (n <= 1) {
         return false;
-    else if (n <= 3)
+    }
+    else if (n <= 3) {
         return true;
-
-    else if (n % 2 == 0 || n % 3 == 0)
+    }
+    else if (n % 2 == 0 || n % 3 == 0) {
         return false;
+    }
 
-    for (int i = 5; i * i <= n; i = i + 6)
-        if (n % i == 0 || n % (i + 2) == 0)
+    for (int i = 5; i * i <= n; i = i + 6) {
+        if (n % i == 0 || n % (i + 2) == 0) {
             return false;
-
+        }
+    }
     return true;
 }
 
 int main() {
-
+    cout << isPrime(7) << "\n";
+    cout << isPrime(10) << "\n";
 }
